982de32e1e65e9a37779c8e60d56ac10fb514541
Aaron Krajeski <aaronkrajeski@gmail.com>
2013-10-07 15:37:56 -0400

    Cleaned up the file structure.

--- setup.py
+++ setup.py
@@ -6,9 +6,11 @@ if 'py2exe' in sys.argv: import py2exe
 if 'py2app' in sys.argv: import py2app
 
 data_files = [('',['js']),
-              ('css',['css/style.css']),
-              ('',['images']),
+              ('',['includes']),
+              ('',['css']),
+              ('',['images'])
               ]
+
 options = {'argv_emulation': False, 'iconfile': 'images/libmapper.icns'}
 
 setup(name='WebMapper',
